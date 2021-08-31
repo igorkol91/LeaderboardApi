0ccebde5d079eeecd63be6ab275091a7a238ed2a
Igor.Koloski <koloski_igor@hotmail.com>
2021-08-31 17:31:21 +0200

    Fixed sorting of leaderboard scores from highest to lowest

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -2,8 +2,8 @@ const resultContainer = document.querySelector('.scores-div');
 
 const refreshDom = (array) => {
   resultContainer.innerHTML = '';
-  const newArray = array.result;
-  newArray.sort();
+  let newArray = array.result;
+  newArray = newArray.sort((a, b) => b.score - a.score);
   (newArray).forEach((element) => {
     const userOutput = document.createElement('p');
     userOutput.classList = ('font-weight m-3');
